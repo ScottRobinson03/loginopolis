810937b3a069bddebc422b6e1c20d2e2324bf6a0
Scott Robinson <scott.robinson2@sky.uk>
2023-04-12 11:47:36 +0100

    Convert db/seedFn.js to 4-space indents

--- db/seedFn.js
+++ db/seedFn.js
@@ -3,8 +3,8 @@ const {User} = require('./');
 const users = require('./seedData');
 
 const seed = async () => {
-  await sequelize.sync({ force: true }); // recreate db
-  await User.bulkCreate(users);
+    await sequelize.sync({ force: true }); // recreate db
+    await User.bulkCreate(users);
 };
 
 module.exports = seed;
